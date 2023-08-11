463fb93a8741ab276b4c858291d0dd229c99e534
xuwei <franklxw@outlook.com>
2023-08-11 16:49:18 +0800

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+    set -g fish_greeting
     source (/usr/bin/starship init fish --print-full-init | psub)
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
