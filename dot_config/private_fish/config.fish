024e949e84826f87f4fe154322d7e0185294279d
xuwei <franklxw@outlook.com>
2024-06-25 11:19:39 +0800

    Update .config/fish/config.fish Update .config/fish/fish_variables

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     set -g fish_greeting
     set -gx EDITOR nvim
     fish_add_path $HOME/.nix-profile/bin /nix/var/nix/profiles/default/bin /run/system-manager/sw/bin/ /nix/var/nix/profiles/default/bin /home/linuxbrew/.linuxbrew/bin
+    fish_add_path $HOME/.local/share/miniconda3/bin
     source (starship init fish --print-full-init | psub)
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
