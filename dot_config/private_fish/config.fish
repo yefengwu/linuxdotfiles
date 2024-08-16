6a1bb88b5c38309fb682616a1672efa3505f24c6
xuwei <franklxw@outlook.com>
2024-08-16 16:53:48 +0800

    Update .config/btop/btop.conf Update .config/lazygit/config.yml Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,10 @@ if status is-interactive
     set -gx EDITOR nvim
     fish_add_path $HOME/.nix-profile/bin /nix/var/nix/profiles/default/bin /run/system-manager/sw/bin/ /nix/var/nix/profiles/default/bin /home/linuxbrew/.linuxbrew/bin
     fish_add_path $HOME/.local/share/miniconda3/bin
+    set -x HOMEBREW_BREW_GIT_REMOTE https://mirrors.ustc.edu.cn/brew.git
+    set -x HOMEBREW_CORE_GIT_REMOTE https://mirrors.ustc.edu.cn/homebrew-core.git
+    set -x HOMEBREW_BOTTLE_DOMAIN https://mirrors.ustc.edu.cn/homebrew-bottles
+    set -x HOMEBREW_API_DOMAIN https://mirrors.ustc.edu.cn/homebrew-bottles/api
     source (starship init fish --print-full-init | psub)
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
