4d8dbb087dce3a5878df40054bef57a2d9bd1d07
xuwei <franklxw@outlook.com>
2025-07-16 03:30:48 +0000

    Update .config/git/config Update .config/lazygit/config.yml Update .config/fish/aliases.fish Update .config/fish/config.fish Update .config/fish/fish_variables

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -19,3 +19,14 @@ if status is-interactive
     bind \b backward-kill-word
 end
 
+function lf
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+function ze 
+    cd (zoxide query -i) 
+end
